add5a26c39ac939f49d78381842696e2c5773de8
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-04-01 16:36:24 -0700

    Animate iPhone button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
-   $('.js-wp-2').waypoint(function(direction){
-      $('.js-wp-2').addClass('animated slideInUp');
-   }, {
-       offset: '70%'
-   });
+$(document).ready(function () {
+    $('.js-wp-2').waypoint(function (direction) {
+        $('.js-wp-2').addClass('animated slideInUp');
+    }, {
+        offset: '70%'
+    });
+    
+    /* With a delay of 2.3 seconds, move this div and animate it from its current position by 3 px */
+    $(".iphone-btn").delay(2300).animate({
+        top: '+=2px'
+    }, 300);
+    $(".iphone-btn").delay(1300).animate({
+        top: '+=-3px'
+    }, 300);
 });
